efe9db0b4539a7aba887cdac19eadfee4da8f8b9
SITARNET <sitarnet@ukr.net>
2023-04-17 16:34:24 +0300

    2. Testing 1-2-3

--- python_5_kyu.py
+++ python_5_kyu.py
@@ -9,10 +9,27 @@
 
 # two_sum([1, 2, 3], 4) # returns [0, 2] or [2, 0]
 
-def two_sum(nums, t):
-    for i, x in enumerate(nums):
-        for j, y in enumerate(nums):
-            if i != j and x + y == t:
-                return [i, j]
+# def two_sum(nums, t):
+#     for i, x in enumerate(nums):
+#         for j, y in enumerate(nums):
+#             if i != j and x + y == t:
+#                 return [i, j]
+#
+# print(two_sum([1, 2, 3], 4))
 
-print(two_sum([1, 2, 3], 4))
+# 2.
+
+# Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+# Write a function which takes a list of strings and returns each line prepended by the correct number.
+# The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+# Examples: (Input --> Output)
+
+def number(lines):
+    count = 1
+    line = []
+    for i in lines:
+        line.append(str(count) + ': ' + i)
+        count += 1
+    return line
+
+print(number(["a", "b", "c"]))
